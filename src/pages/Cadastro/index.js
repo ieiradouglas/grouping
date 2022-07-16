176c504bbbcf7f2407a94bcc50e9a9a9c944f48a
Douglas Vieira da Silva <ieiradouglas@gmail.com>
2022-07-16 10:00:37 -0300

    Alteração do placeholder no campo de cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -13,7 +13,7 @@ function Cadastro() {
                 <form onSubmit={(event)=>event.preventDefault()}>
                     <Input type="text" placeholder="Primeiro Nome"/>
                     <Input type="e-mail" placeholder="Seu melhor E-mail"/>
-                    <Input type="password" placeholder="Digite sua senha"/>
+                    <Input type="password" placeholder="Cadastre uma senha"/>
                     <Button text="Cadastrar"/>
                 </form>
             </div>
